3b8488c9f9ea7fa298f0dece0b89731f06a3ed3c
TIMcre <92308299+TIMcre@users.noreply.github.com>
2022-08-11 11:31:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("more0");
+button.onclick = function(){
+    document.getElementById("info0").style.display = "none";
+}
+
+var button = document.getElementById("less0");
+button.onclick = function(){
+    document.getElementById("info0").style.display = "block";
+}
