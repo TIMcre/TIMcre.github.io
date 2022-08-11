73a5a82ccb91533ca1b6d6ae8d1848a37d6d0c4f
TIMcre <nhom@web.de>
2022-08-11 12:02:53 +0200

    updatet

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var button = document.getElementById("more0");
 button.onclick = function(){
-    document.getElementById("info0").style.display = "none";
+    document.getElementById("info0").style.display = "block";
 }
 
 var button = document.getElementById("less0");
 button.onclick = function(){
-    document.getElementById("info0").style.display = "block";
+    document.getElementById("info0").style.display = "none";
 }
